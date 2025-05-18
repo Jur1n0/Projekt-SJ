a7c7403db504b669e7a1a643c1e52c629dafd4ac
Jur1n0 <danis.juraj16@gmail.com>
2025-05-18 22:03:29 +0200

    Prepojenie databázy

---
+++ fly-master/components/footer.php
@@ -0,0 +1,165 @@
+<footer class="footer" style="background-image: url('./assets/images/footer-bg.png')">
+    <div class="container">
+
+        <div class="footer-top section grid-list">
+
+            <div class="footer-brand">
+
+                <a href="#" class="logo">
+                    <img src="./assets/images/logo.svg" width="102" height="48" alt="fly logo">
+                </a>
+
+                <p class="footer-text">
+                    Private jet charters save your time and give you comfort.
+                </p>
+
+            </div>
+
+            <ul class="footer-list">
+
+                <li>
+                    <p class="footer-list-title">Explore</p>
+                </li>
+
+                <li>
+                    <a href="#" class="footer-link">About</a>
+                </li>
+
+                <li>
+                    <a href="#" class="footer-link">Private Jet Catering</a>
+                </li>
+
+                <li>
+                    <a href="#" class="footer-link">Destinations</a>
+                </li>
+
+                <li>
+                    <a href="#" class="footer-link">Flight Search</a>
+                </li>
+
+                <li>
+                    <a href="#" class="footer-link">Book Flight</a>
+                </li>
+
+            </ul>
+
+            <ul class="footer-list">
+
+                <li>
+                    <p class="footer-list-title">Links</p>
+                </li>
+
+                <li>
+                    <a href="#" class="footer-link">Terms of Use</a>
+                </li>
+
+                <li>
+                    <a href="#" class="footer-link">Contact</a>
+                </li>
+
+                <li>
+                    <a href="#" class="footer-link">News & Press</a>
+                </li>
+
+                <li>
+                    <a href="#" class="footer-link">Games</a>
+                </li>
+
+                <li>
+                    <a href="#" class="footer-link">FAQs</a>
+                </li>
+
+            </ul>
+
+            <ul class="footer-list">
+
+                <li>
+                    <p class="footer-list-title">Contact</p>
+                </li>
+
+                <li class="footer-list-item">
+                    <ion-icon name="call" aria-hidden="true"></ion-icon>
+
+                    <a href="tel:+0123456780" class="footer-link">+01 ( 2345 ) - 6780</a>
+                </li>
+
+                <li class="footer-list-item">
+                    <ion-icon name="mail" aria-hidden="true"></ion-icon>
+
+                    <a href="mailto:fly@info.com" class="footer-link">fly@info.com</a>
+                </li>
+
+                <li class="footer-list-item">
+                    <ion-icon name="location" aria-hidden="ture"></ion-icon>
+
+                    <address class="address">
+                        30 broklyn golden street. New York
+                    </address>
+                </li>
+
+            </ul>
+
+        </div>
+
+        <div class="footer-bottom">
+
+            <p class="copyright">
+                Copyright 2022 by codewithsadee.
+            </p>
+
+            <ul class="social-list">
+
+                <li>
+                    <a href="#" class="social-link">
+                        <ion-icon name="logo-twitter"></ion-icon>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#" class="social-link">
+                        <ion-icon name="logo-facebook"></ion-icon>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#" class="social-link">
+                        <ion-icon name="logo-pinterest"></ion-icon>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#" class="social-link">
+                        <ion-icon name="logo-instagram"></ion-icon>
+                    </a>
+                </li>
+
+            </ul>
+
+        </div>
+
+    </div>
+</footer>
+
+<!--
+     - custom css link
+   -->
+<link rel="stylesheet" href="./assets/css/style.css">
+
+
+<!--
+   - #BACK TO TOP
+ -->
+
+<a href="#top" class="back-top-btn" aria-label="back to top" data-back-top-btn>
+    <ion-icon name="arrow-up-outline" aria-hidden="true"></ion-icon>
+</a>
+<!--
+   - custom js link
+ -->
+<script src="./assets/js/script.js" defer></script>
+
+<!--
+  - ionicon link
+-->
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
